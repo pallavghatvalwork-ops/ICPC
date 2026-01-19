7799442e890c452fad8ef43183edda133799be80
Pallav Ghatval <pallavghatval.work@gmail.com>
2026-01-19 12:38:53 +0530

    Add files via upload

---
+++ Leetcode/Practice_problems/sliding window/Longest Substring with K Uniques.cpp
@@ -0,0 +1,58 @@
+/*
+
+
+Longest Substring with K Uniques
+Difficulty: MediumAccuracy: 34.65%Submissions: 256K+Points: 4
+You are given a string s consisting only lowercase alphabets and an integer k. Your task is to find the length of the longest substring that contains exactly k distinct characters.
+
+Note : If no such substring exists, return -1. 
+
+Examples:
+
+Input: s = "aabacbebebe", k = 3
+Output: 7
+Explanation: The longest substring with exactly 3 distinct characters is "cbebebe", which includes 'c', 'b', and 'e'.
+Input: s = "aaaa", k = 2
+Output: -1
+Explanation: There's no substring with 2 distinct characters.
+Input: s = "aabaaab", k = 2
+Output: 7
+Explanation: The entire string "aabaaab" has exactly 2 unique characters 'a' and 'b', making it the longest valid substring.
+Constraints:
+1 = s.size() = 105
+1 = k = 26
+
+*/
+
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // code here
+        int n=s.length();
+        int res=-1;
+        unordered_map<char,int>f;
+        int low=0;
+        for(int high=0;high<n;high++){
+            
+            f[s[high]]++;
+            
+            while(f.size()>k){
+                f[s[low]]--;
+                if(f[s[low]]==0){
+                    f.erase(s[low]);
+                }
+                
+                low++;
+            }
+            
+            if(f.size()==k){
+                int len=high-low+1;
+                res=max(len,res);
+            }
+            
+        }
+        
+        return res;
+    }
+};
