1215f9968bcd99ffc9655a29951fbcb182d95309
Pallav Ghatval <pallavghatval.work@gmail.com>
2026-02-10 19:56:06 +0530

    Add files via upload

---
+++ Leetcode/Practice_problems/merge intervals/Interval List Intersections.cpp
@@ -0,0 +1,81 @@
+/*
+
+986. Interval List Intersections
+Medium
+Topics
+premium lock icon
+Companies
+You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+ 
+
+Example 1:
+
+
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Example 2:
+
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+ 
+
+Constraints:
+
+0 <= firstList.length, secondList.length <= 1000
+firstList.length + secondList.length >= 1
+0 <= starti < endi <= 109
+endi < starti+1
+0 <= startj < endj <= 109 
+endj < startj+1
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int i=0,j=0;
+
+        vector<vector<int>> res;
+        int n=firstList.size(),m=secondList.size();
+
+        while(i<n && j<m){
+            int start1=firstList[i][0];
+            int end1=firstList[i][1];
+
+            int start2=secondList[j][0];
+            int end2=secondList[j][1];
+
+            if(start1<=start2){
+                if(end1>=start2){
+                    int s=max(start1,start2);
+                    int e=min(end1,end2);
+
+                    res.push_back({s,e});
+                }
+            
+            }else{
+                if(end2>=start1){
+                    int s=max(start1,start2);
+                    int e=min(end1,end2);
+
+                    res.push_back({s,e});
+                }
+            }
+
+            if(end1<=end2){
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return res;
+    }
+};
